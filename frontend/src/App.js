80fa9d466025c03543c3f4a5afc653dacab42f79
cgarbs <christian.garba@gmail.com>
2022-10-15 19:01:06 +0300

    done

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import Home from "./components/Home";
+import "./App.css";
+
+
+
+
+class App extends Component {
+  state = {
+  };
+
+  
+  render() {
+    return (
+      <div className="App">
+        <Home />
+      </div>
+    );
+  }
+}
+export default App;
